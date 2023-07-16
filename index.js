a0b753b7374ebf8b507e76d6db993d8cae3c835c
Alex <Alex3388@live.ca>
2023-07-16 01:51:44 -0700

    Add rps script

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-console.log("hello")
+function getCompChoice() {
+    let hand = ["rock", "paper", "scissors"];
+    let compChoice = hand[Math.floor(Math.random() * hand.length)];
+    return compChoice;
+}
+
+const compHand = getCompChoice()
+const playerChoice = "scissors"
+const playerHand = (playerChoice.toLowerCase())
+
+console.log(playerHand)
+console.log(compHand)
+
+function playRound () {
+    if (playerHand === "rock") {
+        playRoundRock();
+    } else if (playerHand === "paper") {
+        playRoundPaper();
+    } else if (playerHand === "scissors") {
+        playRoundScissors();
+    }
+}
+
+function playRoundRock() {
+    if (compHand === "rock") {
+        console.log("DRAW!!");
+    } else if (compHand === "paper") {
+        console.log("You Lose!!");
+    } else {
+        console.log("You Win!!");
+    }
+}
+
+function playRoundPaper() {
+    if (compHand === "paper") {
+        console.log("DRAW!!");
+    } else if (compHand === "scissors") {
+        console.log("You Lose!!");
+    } else {
+        console.log("You Win!!");
+    }
+}
+
+function playRoundScissors() {
+    if (compHand === "scissors") {
+        console.log("DRAW!!");
+    } else if (compHand === "rock") {
+        console.log("You Lose!!");
+    } else {
+        console.log("You Win!!");
+    }
+}
+
+
+
+// p > r > s 
+
